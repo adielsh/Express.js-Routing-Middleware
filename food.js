150707605a4cf41a2362f64be311e5a30f75f20f
adielsh <adielvl@gmail.com>
2017-03-05 01:58:19 +0200

    more..

--- food.js
+++ food.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/:some', function(req, res) {
   res.send('Its good to eat '+req.params.some);
-  console.log(+req.query.first+ +req.query.sec)
+  console.log(+req.query.first+ +req.query.sec);
 });
 
 
