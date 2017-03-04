971a834f3531e34fccd4b937d646153d9b3b258b
adielsh <adielvl@gmail.com>
2017-03-05 01:45:22 +0200

    expressHelloWorld
    trying

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var food = require ('./food')
+
+var app = express();
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+app.use('/food', food);
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/:name', function (req, res) {
+  res.send('Hello '+req.params.name);
+});
